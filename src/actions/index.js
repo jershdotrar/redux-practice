b9268b4034f1f8a3b8cb129b5655a4a8b0349893
jersh <zombine.rar@gmail.com>
2020-09-18 06:54:14 -0500

    added users reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,5 +4,13 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
   };
+
+  // need a separate api call to retrieve user info
+  export const fetchUser = (id) => async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`); 
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+  }
+  
